34d89358e5fb7817996eeda47b9fdf2a3b66394f
Rafael <rafael.ferreira@viannasempre.com.br>
2021-11-22 16:32:19 -0300

    criada fragment de cadastro de usuario

---
+++ app/src/main/java/com/vianna/entregasapp/ui/usuario/UsuarioAddFragment.java
@@ -0,0 +1,37 @@
+package com.vianna.entregasapp.ui.usuario;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.vianna.entregasapp.R;
+
+public class UsuarioAddFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        return null;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
+}
